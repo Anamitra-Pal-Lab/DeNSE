10167e315e88292c0b70ce69ff680f004ca7adbf
hritikshah345 <73395005+hritikshah345@users.noreply.github.com>
2024-01-15 08:50:40 -0500

    Update Data_Loading.py

--- Data_Loading.py
+++ Data_Loading.py
@@ -57,3 +57,22 @@ def DeNSE_data_loader_testing_data():
     df_VDATA_ang = pd.read_csv('VDATA_ang_T1_28000.csv', header = None)
     
     return(df_If_mag, df_If_ang, df_It_mag,  df_It_ang, df_VDATA_mag, df_VDATA_ang)
+
+def Load_Training_Data(filepath, n):
+    
+    # Reading the current and voltage data
+    # Reading the voltages and current measurements to pandas daraframes
+    # If - from bus current
+    # It - to bus current
+    # V - voltage
+    # mag - magnitude
+    # ang - phase angle  
+
+    df_If_ang = pd.read_csv(filepath+('If_ang_train_T'+str(n)+'_12am.csv'), header = None)
+    df_If_mag= pd.read_csv(filepath+('If_mag_train_T'+str(n)+'_12am.csv'), header = None)
+    df_It_ang= pd.read_csv(filepath+('It_ang_train_T'+str(n)+'_12am.csv'), header = None)
+    df_It_mag= pd.read_csv(filepath+('It_mag_train_T'+str(n)+'_12am.csv'), header = None)
+    df_VDATA_mag = pd.read_csv(filepath+('VDATA_mag_train_T'+str(n)+'_12am.csv'), header = None)
+    df_VDATA_ang = pd.read_csv(filepath+('VDATA_ang_train_T'+str(n)+'_12am.csv'), header = None)
+
+    return(df_VDATA_mag, df_VDATA_ang, df_If_mag, df_If_ang, df_It_mag, df_It_ang, df_From_To_buses_118)
