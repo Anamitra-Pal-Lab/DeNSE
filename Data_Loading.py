e133fa76d9f9730c2587be49bffe98daf10262c5
hritikshah345 <73395005+hritikshah345@users.noreply.github.com>
2024-02-01 23:38:48 -0500

    Update Data_Loading.py

--- Data_Loading.py
+++ Data_Loading.py
@@ -87,15 +87,15 @@ def Load_Testing_Transfer_Learning_Data(filepath, n):
     # mag - magnitude
     # ang - phase angle  
 
-    df_If_ang = pd.read_csv(filepath+('If_ang_test_T'+str(n)+'_12am.csv'), header = None)
-    df_If_mag= pd.read_csv(filepath+('If_mag_test_T'+str(n)+'_12am.csv'), header = None)
-    df_It_ang= pd.read_csv(filepath+('It_ang_test_T'+str(n)+'_12am.csv'), header = None)
-    df_It_mag= pd.read_csv(filepath+('It_mag_test_T'+str(n)+'_12am.csv'), header = None)
-    df_VDATA_mag = pd.read_csv(filepath+('VDATA_mag_test_T'+str(n)+'_12am.csv'), header = None)
-    df_VDATA_ang = pd.read_csv(filepath+('VDATA_ang_test_T'+str(n)+'_12am.csv'), header = None)
-
-    
-
+    df_If_ang = pd.read_csv(filepath+('If_ang_test_T'+str(n)+'_12am.csv'))
+    df_If_mag= pd.read_csv(filepath+('If_mag_test_T'+str(n)+'_12am.csv'))
+    df_It_ang= pd.read_csv(filepath+('It_ang_test_T'+str(n)+'_12am.csv'))
+    df_It_mag= pd.read_csv(filepath+('It_mag_test_T'+str(n)+'_12am.csv'))
+    df_VDATA_mag = pd.read_csv(filepath+('VDATA_mag_test_T'+str(n)+'_12am.csv'))
+    df_VDATA_ang = pd.read_csv(filepath+('VDATA_ang_test_T'+str(n)+'_12am.csv'))
+  
+   
+    print('Read the test csv files')
 
     return(df_VDATA_mag, df_VDATA_ang, df_If_mag, df_If_ang, df_It_mag, df_It_ang)
     
