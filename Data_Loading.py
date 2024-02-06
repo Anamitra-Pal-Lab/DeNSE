727745744b5af07023846b34a8ccc31ed5fc6430
bazimian <69228500+bazimian@users.noreply.github.com>
2024-02-05 19:53:19 -0700

    Update Data_Loading.py

--- Data_Loading.py
+++ Data_Loading.py
@@ -74,8 +74,23 @@ def Load_Training_Data(filepath, n):
     df_It_mag= pd.read_csv(filepath+('It_mag_train_T'+str(n)+'_12am.csv'), header = None)
     df_VDATA_mag = pd.read_csv(filepath+('VDATA_mag_train_T'+str(n)+'_12am.csv'), header = None)
     df_VDATA_ang = pd.read_csv(filepath+('VDATA_ang_train_T'+str(n)+'_12am.csv'), header = None)
+    df_From_To_buses_118 = pd.read_csv(r'From_To_buses_118.csv', header = None)
+    return(df_VDATA_mag, df_VDATA_ang, df_If_mag, df_If_ang, df_It_mag, df_It_ang, df_From_To_buses_118)
+
+def Load_Testing_Data(filepath, n):
+
+    df_If_ang = pd.read_csv(filepath+('If_ang_test_T'+str(n)+'_12am.csv'))
+    df_If_mag= pd.read_csv(filepath+('If_mag_test_T'+str(n)+'_12am.csv'))
+    df_It_ang= pd.read_csv(filepath+('It_ang_test_T'+str(n)+'_12am.csv'))
+    df_It_mag= pd.read_csv(filepath+('It_mag_test_T'+str(n)+'_12am.csv'))
+    df_VDATA_mag = pd.read_csv(filepath+('VDATA_mag_test_T'+str(n)+'_12am.csv'))
+    df_VDATA_ang = pd.read_csv(filepath+('VDATA_ang_test_T'+str(n)+'_12am.csv'))
+  
+   
+    print('Read the test csv files')
 
     return(df_VDATA_mag, df_VDATA_ang, df_If_mag, df_If_ang, df_It_mag, df_It_ang)
+           
 
 def Load_Testing_Transfer_Learning_Data(filepath, n):
     
